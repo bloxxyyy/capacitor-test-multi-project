7cccc57e4c72256a388e9a0202c579797abacf5b
Marco <mn.noppen@hotmail.com>
2025-05-28 00:44:43 +0200

    chore; updated; linting to use prettier

--- mobile-angular-capacitor-app/eslint.config.js
+++ mobile-angular-capacitor-app/eslint.config.js
@@ -2,42 +2,45 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintPluginPrettier = require("eslint-plugin-prettier");
+const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      "prettier/prettier": "error",
+      ...eslintConfigPrettier.rules,
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "app", style: "camelCase" }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "app", style: "kebab-case" }
+      ],
+      "@angular-eslint/no-input-rename": "error",
+      "@angular-eslint/no-output-on-prefix": "error",
+      "@angular-eslint/no-output-rename": "error",
+      "prefer-const": "error"
+    },
+    processor: angular.processInlineTemplates,
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
+      ...angular.configs.tsRecommended
+    ]
   },
   {
     files: ["**/*.html"],
+    rules: {},
     extends: [
       ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
+      ...angular.configs.templateAccessibility
+    ]
   }
 );

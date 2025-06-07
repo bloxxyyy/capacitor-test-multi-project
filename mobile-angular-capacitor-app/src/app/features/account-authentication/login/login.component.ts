c0f9466aac790b37b976e5d1396a079ca80b5343
Marco <mn.noppen@hotmail.com>
2025-06-07 02:01:51 +0200

    Add login button html and action

--- mobile-angular-capacitor-app/src/app/features/account-authentication/login/login.component.ts
+++ mobile-angular-capacitor-app/src/app/features/account-authentication/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../core/services/authentication.service';
+import { UrlConfigurationService } from '../../../core/config/url-configuration.service';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthenticationService);
+  private router = inject(Router);
+  private urlConfig = inject(UrlConfigurationService);
+
+  async onLogin(): Promise<void> {
+    await this.authService.login();
+    await this.router.navigate([this.urlConfig.accountOverview]);
+  }
+}

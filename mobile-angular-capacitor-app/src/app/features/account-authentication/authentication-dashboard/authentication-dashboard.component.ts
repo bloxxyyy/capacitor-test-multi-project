3b282532e4827f82fb3c9c8caa8511e0b4ca5e2a
Marco <mn.noppen@hotmail.com>
2025-06-08 21:03:20 +0200

    Initial Biometrics test

--- mobile-angular-capacitor-app/src/app/features/account-authentication/authentication-dashboard/authentication-dashboard.component.ts
+++ mobile-angular-capacitor-app/src/app/features/account-authentication/authentication-dashboard/authentication-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NativeBiometric } from "@capgo/capacitor-native-biometric";
 
 @Component({
   selector: 'app-authentication-dashboard',
@@ -6,4 +7,44 @@ import { Component } from '@angular/core';
   templateUrl: './authentication-dashboard.component.html',
   styleUrl: './authentication-dashboard.component.scss',
 })
-export class AuthenticationDashboardComponent {}
+export class AuthenticationDashboardComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.performBiometricVerification();
+  }
+
+  async performBiometricVerification() {
+    const result = await NativeBiometric.isAvailable();
+
+    if(!result.isAvailable) return;
+
+    //const isFaceID = result.biometryType == BiometryType.FACE_ID;
+
+    const verified = await NativeBiometric.verifyIdentity({
+      reason: "For easy log in",
+      title: "Log in",
+      subtitle: "Maybe add subtitle here?",
+      description: "Maybe a description too?",
+    })
+      .then(() => true)
+      .catch(() => false);
+
+    if(!verified) return;
+
+    NativeBiometric.setCredentials({
+      username: "usernameTest",
+      password: "passwordTest",
+      server: "www.example.com",
+    }).then();
+
+    const credentials = await NativeBiometric.getCredentials({
+      server: "www.example.com",
+    });
+
+    console.log('Biometric credentials:', credentials);
+
+    //await this.router.navigate(['/login']);
+
+  }
+
+}

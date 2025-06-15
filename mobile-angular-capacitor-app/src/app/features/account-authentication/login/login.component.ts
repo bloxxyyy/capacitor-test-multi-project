37a9e9dde1b3f97330e0d325466834886f99684a
Marco <mn.noppen@hotmail.com>
2025-06-16 00:25:59 +0200

    The login component now should only call the Authentication service.

--- mobile-angular-capacitor-app/src/app/features/account-authentication/login/login.component.ts
+++ mobile-angular-capacitor-app/src/app/features/account-authentication/login/login.component.ts
@@ -1,8 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AccountIdRepository } from '../../../core/repositories/accountId.repository';
-import { UrlConfigurationService } from '../../../core/config/url-configuration.service';
-import { BiometricsService } from '../../../core/services/biometrics.service';
+import { Component, inject } from '@angular/core';
 import { AuthenticationService } from '../../../core/services/authentication.service';
 
 @Component({
@@ -11,27 +7,10 @@ import { AuthenticationService } from '../../../core/services/authentication.ser
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent implements OnInit {
-  private accountIdRepo = inject(AccountIdRepository);
-  private router = inject(Router);
-  private urlConfig = inject(UrlConfigurationService);
-  private biometricsService = inject(BiometricsService);
+export class LoginComponent {
   private authenticationService = inject(AuthenticationService);
 
-  async ngOnInit(): Promise<void> {
-    const hasAccountId = await this.accountIdRepo.hasAccountId();
-    const hasBiometricEnabled = await this.biometricsService.hasBiometricEnabled();
-    const isReopenedApp = this.biometricsService.isAppResumedFromBackground();
-
-    if (hasAccountId && hasBiometricEnabled && isReopenedApp) {
-      const isVerifiedWithBiometrics = await this.biometricsService.tryVerifyWithBiometrics();
-      if (isVerifiedWithBiometrics) {
-        await this.router.navigate([this.urlConfig.accountOverview]);
-      }
-    }
-  }
-
-  async onLogin(): Promise<void> {
-    await this.authenticationService.onLogin();
+  async onLoginWithAccountInformation(): Promise<void> {
+    await this.authenticationService.onLoginWithAccountInformation();
   }
 }

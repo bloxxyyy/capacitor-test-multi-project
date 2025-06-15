8c6f690a3f00f104cf1c2fe4855208072a29b078
Marco <mn.noppen@hotmail.com>
2025-06-16 00:25:02 +0200

    For now we disable the isAppResumed check. This will break the code when ask for biometrics and then cancelling. As it wont re-ask but now we can still continue in the route.

--- mobile-angular-capacitor-app/src/app/core/guards/authentication/requires-authentication.guard.ts
+++ mobile-angular-capacitor-app/src/app/core/guards/authentication/requires-authentication.guard.ts
@@ -4,7 +4,7 @@ import { UserRole } from '../../enums/user-role';
 import { AccountRolesRepository } from '../../repositories/accountRoles.repository';
 import { UrlConfigurationService } from '../../config/url-configuration.service';
 import { AccountIdRepository } from '../../repositories/accountId.repository';
-import { BiometricsService } from '../../services/biometrics.service';
+//import { BiometricsService } from '../../services/biometrics.service';
 
 export function requiresAuthenticationGuard(requiredRoles: UserRole[] = []): CanActivateFn {
   return async () => {
@@ -12,7 +12,7 @@ export function requiresAuthenticationGuard(requiredRoles: UserRole[] = []): Can
     const accountRolesRepo = inject(AccountRolesRepository);
     const router = inject(Router);
     const urlConfigurationService = inject(UrlConfigurationService);
-    const biometricsService = inject(BiometricsService);
+    //const biometricsService = inject(BiometricsService);
 
     // First check if we already made an account, else we always have to login or create and account first.
     const hasAccountId = await accountIdRepo.hasAccountId();
@@ -22,11 +22,11 @@ export function requiresAuthenticationGuard(requiredRoles: UserRole[] = []): Can
     }
 
     // Next check if we have done biometrics for this openedAndForground app session
-    const appResumedFromBackground = biometricsService.isAppResumedFromBackground();
-    if (appResumedFromBackground) {
-      const loginUrl = urlConfigurationService.loginPath;
-      return router.createUrlTree([loginUrl]);
-    }
+    //const appResumedFromBackground = biometricsService.isAppResumedFromBackground();
+    //if (appResumedFromBackground) {
+    //  const loginUrl = urlConfigurationService.loginPath;
+    //  return router.createUrlTree([loginUrl]);
+   // }
 
     // Next check if there are roles required
     if (requiredRoles.length < 1) return true;
